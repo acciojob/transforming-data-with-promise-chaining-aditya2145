a86f7818760c982b73fb529573e1467552f40e9a
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2024-10-16 09:58:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
 //your JS code here. If required.
+const output = document.getElementById('output');
+const button = document.getElementById('btn');
+
+function transformData() {
+	const input = document.getElementById('ip').value;
+	const promise = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(input);
+			output.textContent = `Result: ${input}`;
+		}, 2000)
+	});
+
+	promise.then((message) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				output.textContent = `Result: ${message}`;
+				resolve(message);
+			}, 2000)
+		});
+	})
+	.then((message) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				output.textContent = `Result: ${message*2}`;
+				resolve(message*2);
+			}, 1000)
+		});
+	})
+	.then((message) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				output.textContent = `Result: ${message-3}`;
+				resolve(message-3);
+			}, 1000)
+		});
+	})
+	.then((message) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				output.textContent = `Result: ${message/2}`;
+				resolve(message/2);
+			}, 1000)
+		});
+	})
+	.then((message) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				output.textContent = `Result: ${message+10}`;
+				resolve(message+10);
+			}, 1000)
+		});
+		
+	})
+	.then((message) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				output.textContent = `Final Result: ${message}`;
+				resolve(message);
+			}, 1000)
+		});
+	})
+	.catch((err) => {
+      output.textContent = `Error: ${err}`;
+    });
+}
+
+button.addEventListener('click', transformData);
